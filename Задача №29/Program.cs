68b94df8e1bcd2c794195a28112c30a9c5c88dca
DariyaSazonova <Sazonovade@gmail.com>
2023-01-18 21:19:32 +0300

    Заказа №29

---
+++ Задача №29/Program.cs
@@ -0,0 +1,41 @@
+﻿/* Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. 
+Данные вводятся с консоли пользователем */
+
+int [] arr = { 0, 0, 0, 0, 0, 0, 0, 0};
+
+// метод ввода массива
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < 8; i++)
+    {
+        Console.WriteLine($"{array[i] }");
+    }
+    Console.WriteLine();
+}
+
+// метод формирования массива
+void SelectionSort(int[] array)
+{
+    int num = 1;
+    string h = string.Empty;
+
+    for (int i = 0; i < 8; i++)
+    {
+        if (num == 1) h = "первый";
+        if (num == 2) h = "второй";
+        if (num == 3) h = "третий";
+        if (num == 4) h = "четвертый";
+        if (num == 5) h = "пятый";
+        if (num == 6) h = "шестой";
+        if (num == 7) h = "седьмой";
+        if (num == 8) h = "восьмой";
+
+        Console.Write($"Введите {h} элемент массива: ");
+        array[i] = int.Parse(Console.ReadLine());
+        num +=1;
+    }
+    Console.WriteLine();
+}
+SelectionSort(arr);
+PrintArray(arr);
